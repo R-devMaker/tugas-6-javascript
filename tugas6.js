9c9175441262073596ee4896980520116ec80f23
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 13:55:36 +0700

    Tugas 6 JavaScript
    Pengumpulan tugas 6 JavaScript Niomic

---
+++ tugas6.js
@@ -0,0 +1,49 @@
+for (var i = 0; i <= 20; i++){
+    if (i % 2 == 0) {
+        if (i == 0) {
+            console.log("nol");
+        } else if (i == 1){
+            console.log("satu");
+        } else if (i == 2) {
+            console.log("dua");
+        } else if (i == 3) {
+            console.log("tiga");
+        } else if (i == 4) {
+            console.log("empat");
+        } else if (i == 5) {
+            console.log("lima");
+        } else if (i == 6) {
+            console.log("enam");
+        } else if (i == 7) {
+            console.log("tujuh");
+        } else if (i == 8) {
+            console.log("delapan");
+        } else if (i == 9) {
+            console.log("sembilan");
+        } else if (i == 10) {
+            console.log("sepuluh");
+        } else if (i == 11) {
+            console.log("sebelas");
+        } else if (i == 12) {
+            console.log("dua belas");
+        } else if (i == 13) {
+            console.log("tiga belas");
+        } else if (i == 14) {
+            console.log("empat belas");
+        } else if (i == 15) {
+            console.log("lima belas");
+        } else if (i == 16) {
+            console.log("enam belas");
+        } else if (i == 17) {
+            console.log("tujuh belas");
+        } else if (i == 18) {
+            console.log("delapan belas");
+        } else if (i == 19) {
+            console.log("sembilan belas");
+        } else if (i == 20) {
+            console.log("dua puluh");
+        }
+    } else {
+        console.log(i);
+    }
+}
